8c97c8c1afd1058c8368e2e7b43f5da07c689e33
MohamedNouaoui <mohamednouaoui5@gmail.com>
2025-01-21 11:07:59 +0100

    deleted frolde

--- Miam-App-BackEnd/src/main/java/com/miam/miamappbackend/Repository/MealsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.miam.miamappbackend.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.miam.miamappbackend.Entity.Meals;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MealsRepository extends JpaRepository<Meals,Long> {
-}
--- Miam-App-BackEnd/src/main/java/com/miam/miamappbackend/Repository/RestaurantRepository.java
+++
@@ -1,8 +0,0 @@
-package com.miam.miamappbackend.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.miam.miamappbackend.Entity.Restaurant;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RestaurantRepository extends JpaRepository<Restaurant,Long> {
-}
