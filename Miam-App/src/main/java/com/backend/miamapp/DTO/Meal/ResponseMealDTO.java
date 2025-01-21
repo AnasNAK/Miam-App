fbb89e9f145a0bc1032681e7e6b9772a4e898b72
MohamedNouaoui <mohamednouaoui5@gmail.com>
2025-01-21 12:38:22 +0100

    made some changes

--- Miam-App/src/main/java/com/backend/miamapp/DTO/Meal/ResponseMealDTO.java
+++ Miam-App/src/main/java/com/backend/miamapp/DTO/Meal/ResponseMealDTO.java
@@ -1,12 +1,26 @@
 package com.backend.miamapp.DTO.Meal;
 
+import com.backend.miamapp.Entity.Enum.MealCategory;
+import com.backend.miamapp.Entity.Restaurant;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.Duration;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ResponseMealDTO {
-
+    private Long id;
+    private String name;
+    private String description;
+    private Duration preparationTime;
+    private double price;
+    private int quantity;
+    private MealCategory category;
+    private Restaurant restaurant;
+    private
 }
