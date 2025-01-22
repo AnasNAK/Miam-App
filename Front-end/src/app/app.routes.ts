f75a7f2a35006eeaeed10ca74ee8229a25cb55a3
MohamedNouaoui <mohamednouaoui5@gmail.com>
2025-01-22 13:08:07 +0100

    all

--- Front-end/src/app/app.routes.ts
+++ Front-end/src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from '../pages/home/home-page.component';
 import { ContentLayoutComponent } from '../layouts/content-layout/content-layout.component';
+import { OrderPageComponent } from '../pages/order-page/order-page.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: ContentLayoutComponent,
     loadChildren : () => import('../pages/home/home.routes').then(r => r.routes)
+  },
+  {
+    path:'order',
+    component:OrderPageComponent
   },
 ];
