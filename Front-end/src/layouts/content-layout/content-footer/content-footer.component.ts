36c4443c4bac6af2709db83e48ef9d2446994989
Abdelouahed-SENANE <abdosenane75@gmail.com>
2025-01-22 09:27:14 +0100

    [GDR-45] feature: add nav bar  and layout component to the project

---
+++ Front-end/src/layouts/content-layout/content-footer/content-footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../../../shared/components/navbar/navbar.component';
+
+@Component({
+  selector: 'app-content-footer',
+  imports: [NavbarComponent],
+  templateUrl: './content-footer.component.html',
+  styleUrl: './content-footer.component.css'
+})
+export class ContentFooterComponent {
+
+}
