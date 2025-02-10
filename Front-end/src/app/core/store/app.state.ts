f1dcc74bfd5471c83b54668b633946ada0bb82c7
Joual27 <endy.panda@Outlook.com>
2025-02-10 15:27:37 +0100

    bux fixed

--- Front-end/src/app/core/store/app.state.ts
+++ Front-end/src/app/core/store/app.state.ts
@@ -1,11 +1,9 @@
 import { MealsInterface } from "../../../shared/models/state.models";
-import { BestSellersInterface } from "../../../entities/meals/models/meals.models";
 import { AuthState } from "../auth/store/auth.models";
 
 
 export interface AppState {
     order: any;
     bestSellers: MealsInterface;
-    bestSellers: BestSellersInterface;
     auth : AuthState
 }
